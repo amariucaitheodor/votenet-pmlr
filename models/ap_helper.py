a5dd33df0e708e96ec721b4ad5f40bfaf0757690
Lucas Cosier <9642742+luke-ck@users.noreply.github.com>
2022-05-29 16:15:07 +0200

    testing differences with original

--- models/ap_helper.py
+++ models/ap_helper.py
@@ -334,3 +334,61 @@ class APCalculator(object):
         self.gt_map_cls = {}  # {scan_id: [(classname, bbox)]}
         self.pred_map_cls = {}  # {scan_id: [(classname, bbox, score)]}
         self.scan_cnt = 0
+
+
+class VotenetAPCalculator(object):
+    ''' Calculating Average Precision '''
+
+    def __init__(self, ap_iou_thresh=0.25, class2type_map=None):
+        """
+        Args:
+            ap_iou_thresh: float between 0 and 1.0
+                IoU threshold to judge whether a prediction is positive.
+            class2type_map: [optional] dict {class_int:class_name}
+        """
+        self.ap_iou_thresh = ap_iou_thresh
+        self.class2type_map = class2type_map
+        self.reset()
+
+    def step(self, batch_pred_map_cls, batch_gt_map_cls):
+        """ Accumulate one batch of prediction and groundtruth.
+
+        Args:
+            batch_pred_map_cls: a list of lists [[(pred_cls, pred_box_params, score),...],...]
+            batch_gt_map_cls: a list of lists [[(gt_cls, gt_box_params),...],...]
+                should have the same length with batch_pred_map_cls (batch_size)
+        """
+
+        bsize = len(batch_pred_map_cls)
+        assert (bsize == len(batch_gt_map_cls))
+        for i in range(bsize):
+            self.gt_map_cls[self.scan_cnt] = batch_gt_map_cls[i]
+            self.pred_map_cls[self.scan_cnt] = batch_pred_map_cls[i]
+            self.scan_cnt += 1
+
+    def compute_metrics(self):
+        """ Use accumulated predictions and groundtruths to compute Average Precision.
+        """
+        rec, prec, ap = eval_det_multiprocessing(self.pred_map_cls, self.gt_map_cls, ovthresh=self.ap_iou_thresh,
+                                                 get_iou_func=get_iou_obb)
+        ret_dict = {}
+        for key in sorted(ap.keys()):
+            clsname = self.class2type_map[key] if self.class2type_map else str(key)
+            ret_dict['%s Average Precision' % (clsname)] = ap[key]
+        ret_dict['mAP'] = np.mean(list(ap.values()))
+        rec_list = []
+        for key in sorted(ap.keys()):
+            clsname = self.class2type_map[key] if self.class2type_map else str(key)
+            try:
+                ret_dict['%s Recall' % (clsname)] = rec[key][-1]
+                rec_list.append(rec[key][-1])
+            except:
+                ret_dict['%s Recall' % (clsname)] = 0
+                rec_list.append(0)
+        ret_dict['AR'] = np.mean(rec_list)
+        return ret_dict
+
+    def reset(self):
+        self.gt_map_cls = {}  # {scan_id: [(classname, bbox)]}
+        self.pred_map_cls = {}  # {scan_id: [(classname, bbox, score)]}
+        self.scan_cnt = 0
--- train.py
+++ train.py
@@ -38,7 +38,7 @@ sys.path.append(os.path.join(ROOT_DIR, 'models'))
 from pytorch_utils import BNMomentumScheduler
 # from tf_visualizer import Visualizer as TfVisualizer
 import wandb
-from ap_helper import APCalculator, parse_predictions, parse_groundtruths
+from ap_helper import APCalculator, VotenetAPCalculator, parse_predictions, parse_groundtruths
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', default='votenet', help='Model file name [default: votenet]')
@@ -49,7 +49,8 @@ parser.add_argument('--dump_dir', default=None, help='Dump dir to save sample ou
 parser.add_argument('--num_point', type=int, default=200000, help='Point Number [default: 20000]')
 parser.add_argument('--num_target', type=int, default=256, help='Proposal number [default: 256]')
 parser.add_argument('--vote_factor', type=int, default=1, help='Vote factor [default: 1]')
-parser.add_argument('--cluster_sampling', default='vote_fps', help='Sampling strategy for vote clusters: vote_fps, seed_fps, random [default: vote_fps]')
+parser.add_argument('--cluster_sampling', default='vote_fps',
+                    help='Sampling strategy for vote clusters: vote_fps, seed_fps, random [default: vote_fps]')
 parser.add_argument('--ap_iou_thresh', type=float, default=0.25, help='AP IoU threshold [default: 0.25]')
 parser.add_argument('--max_epoch', type=int, default=200, help='Epoch to run [default: 180]')
 parser.add_argument('--batch_size', type=int, default=16, help='Batch Size during training [default: 8]')
@@ -57,7 +58,8 @@ parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial 
 parser.add_argument('--weight_decay', type=float, default=0, help='Optimization L2 weight decay [default: 0]')
 parser.add_argument('--bn_decay_step', type=int, default=20, help='Period of BN decay (in epochs) [default: 20]')
 parser.add_argument('--bn_decay_rate', type=float, default=0.1, help='Decay rate for BN decay [default: 0.5]')
-parser.add_argument('--lr_decay_steps', default='80,120,160', help='When to decay the learning rate (in epochs) [default: 80,120,160]')
+parser.add_argument('--lr_decay_steps', default='80,120,160',
+                    help='When to decay the learning rate (in epochs) [default: 80,120,160]')
 parser.add_argument('--lr_decay_rates', default='0.1,0.1,0.1', help='Decay rates for lr decay [default: 0.1,0.1,0.1]')
 parser.add_argument('--no_height', action='store_true', help='Do NOT use height signal in input.')
 parser.add_argument('--use_color', action='store_true', help='Use RGB color in input.')
@@ -75,7 +77,7 @@ BN_DECAY_STEP = FLAGS.bn_decay_step
 BN_DECAY_RATE = FLAGS.bn_decay_rate
 LR_DECAY_STEPS = [int(x) for x in FLAGS.lr_decay_steps.split(',')]
 LR_DECAY_RATES = [float(x) for x in FLAGS.lr_decay_rates.split(',')]
-assert(len(LR_DECAY_STEPS)==len(LR_DECAY_RATES))
+assert (len(LR_DECAY_STEPS) == len(LR_DECAY_RATES))
 LOG_DIR = FLAGS.log_dir
 DEFAULT_DUMP_DIR = os.path.join(BASE_DIR, os.path.basename(LOG_DIR))
 DUMP_DIR = FLAGS.dump_dir if FLAGS.dump_dir is not None else DEFAULT_DUMP_DIR
@@ -86,78 +88,87 @@ FLAGS.DUMP_DIR = DUMP_DIR
 
 # Prepare LOG_DIR and DUMP_DIR
 if os.path.exists(LOG_DIR) and FLAGS.overwrite:
-    print('Log folder %s already exists. Are you sure to overwrite? (Y/N)'%(LOG_DIR))
+    print('Log folder %s already exists. Are you sure to overwrite? (Y/N)' % (LOG_DIR))
     c = input()
     if c == 'n' or c == 'N':
         print('Exiting..')
         exit()
     elif c == 'y' or c == 'Y':
         print('Overwrite the files in the log and dump folers...')
-        os.system('rm -r %s %s'%(LOG_DIR, DUMP_DIR))
+        os.system('rm -r %s %s' % (LOG_DIR, DUMP_DIR))
 
 if not os.path.exists(LOG_DIR):
     os.mkdir(LOG_DIR)
 
 LOG_FOUT = open(os.path.join(LOG_DIR, 'log_train.txt'), 'a')
-LOG_FOUT.write(str(FLAGS)+'\n')
+LOG_FOUT.write(str(FLAGS) + '\n')
+
+
 def log_string(out_str):
-    LOG_FOUT.write(out_str+'\n')
+    LOG_FOUT.write(out_str + '\n')
     LOG_FOUT.flush()
     print(out_str)
+
+
 if not os.path.exists(DUMP_DIR): os.mkdir(DUMP_DIR)
 
-# Init datasets and dataloaders 
+
+# Init datasets and dataloaders
 def my_worker_init_fn(worker_id):
     np.random.seed(np.random.get_state()[1][0] + worker_id)
+
 
 # Create Dataset and Dataloader
 if FLAGS.dataset == 'sunrgbd':
     sys.path.append(os.path.join(ROOT_DIR, 'sunrgbd'))
     from sunrgbd_detection_dataset import SunrgbdDetectionVotesDataset, MAX_NUM_OBJ
     from model_util_sunrgbd import SunrgbdDatasetConfig
+
     DATASET_CONFIG = SunrgbdDatasetConfig()
     TRAIN_DATASET = SunrgbdDetectionVotesDataset('train', num_points=NUM_POINT,
-        augment=True,
-        use_color=FLAGS.use_color, use_height=(not FLAGS.no_height),
-        use_v1=(not FLAGS.use_sunrgbd_v2))
+                                                 augment=True,
+                                                 use_color=FLAGS.use_color, use_height=(not FLAGS.no_height),
+                                                 use_v1=(not FLAGS.use_sunrgbd_v2))
     TEST_DATASET = SunrgbdDetectionVotesDataset('val', num_points=NUM_POINT,
-        augment=False,
-        use_color=FLAGS.use_color, use_height=(not FLAGS.no_height),
-        use_v1=(not FLAGS.use_sunrgbd_v2))
+                                                augment=False,
+                                                use_color=FLAGS.use_color, use_height=(not FLAGS.no_height),
+                                                use_v1=(not FLAGS.use_sunrgbd_v2))
 elif FLAGS.dataset == 'scannet':
     sys.path.append(os.path.join(ROOT_DIR, 'scannet'))
     from scannet_detection_dataset import ScannetDetectionDataset, MAX_NUM_OBJ
     from model_util_scannet import ScannetDatasetConfig
+
     DATASET_CONFIG = ScannetDatasetConfig()
     TRAIN_DATASET = ScannetDetectionDataset('train', num_points=NUM_POINT,
-        augment=True,
-        use_color=FLAGS.use_color, use_height=(not FLAGS.no_height))
+                                            augment=True,
+                                            use_color=FLAGS.use_color, use_height=(not FLAGS.no_height))
     TEST_DATASET = ScannetDetectionDataset('val', num_points=NUM_POINT,
-        augment=False,
-        use_color=FLAGS.use_color, use_height=(not FLAGS.no_height))
+                                           augment=False,
+                                           use_color=FLAGS.use_color, use_height=(not FLAGS.no_height))
 elif FLAGS.dataset == 'apple':
     sys.path.append(os.path.join(ROOT_DIR, 'apple'))
     from model_util_apple import AppleDatasetConfig
     from apple_detection_dataset import AppleDetectionVotesDataset
+
     DATASET_CONFIG = AppleDatasetConfig()
     TRAIN_DATASET = AppleDetectionVotesDataset('train', num_points=NUM_POINT,
-        use_height=(not FLAGS.no_height))
+                                               use_height=(not FLAGS.no_height))
     TEST_DATASET = AppleDetectionVotesDataset('val', num_points=NUM_POINT,
-        use_height=(not FLAGS.no_height))
+                                              use_height=(not FLAGS.no_height))
 else:
-    print('Unknown dataset %s. Exiting...'%(FLAGS.dataset))
+    print('Unknown dataset %s. Exiting...' % (FLAGS.dataset))
     exit(-1)
 print(len(TRAIN_DATASET), len(TEST_DATASET))
 TRAIN_DATALOADER = DataLoader(TRAIN_DATASET, batch_size=BATCH_SIZE,
-    shuffle=True, num_workers=4, worker_init_fn=my_worker_init_fn)
+                              shuffle=True, num_workers=4, worker_init_fn=my_worker_init_fn)
 TEST_DATALOADER = DataLoader(TEST_DATASET, batch_size=BATCH_SIZE,
-    shuffle=True, num_workers=4, worker_init_fn=my_worker_init_fn)
+                             shuffle=True, num_workers=4, worker_init_fn=my_worker_init_fn)
 print(len(TRAIN_DATALOADER), len(TEST_DATALOADER))
 
 # Init the model and optimzier
-MODEL = importlib.import_module(FLAGS.model) # import network module
+MODEL = importlib.import_module(FLAGS.model)  # import network module
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-num_input_channel = int(FLAGS.use_color)*3 + int(not FLAGS.no_height)*1
+num_input_channel = int(FLAGS.use_color) * 3 + int(not FLAGS.no_height) * 1
 
 if FLAGS.model == 'boxnet':
     Detector = MODEL.BoxNet
@@ -174,9 +185,9 @@ net = Detector(num_class=DATASET_CONFIG.num_class,
                sampling=FLAGS.cluster_sampling)
 
 if torch.cuda.device_count() > 1:
-  log_string("Let's use %d GPUs!" % (torch.cuda.device_count()))
-  # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-  net = nn.DataParallel(net)
+    log_string("Let's use %d GPUs!" % (torch.cuda.device_count()))
+    # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
+    net = nn.DataParallel(net)
 net.to(device)
 criterion = MODEL.get_loss
 
@@ -184,33 +195,36 @@ criterion = MODEL.get_loss
 optimizer = optim.Adam(net.parameters(), lr=BASE_LEARNING_RATE, weight_decay=FLAGS.weight_decay)
 
 # Load checkpoint if there is any
-it = -1 # for the initialize value of `LambdaLR` and `BNMomentumScheduler`
+it = -1  # for the initialize value of `LambdaLR` and `BNMomentumScheduler`
 start_epoch = 0
 if CHECKPOINT_PATH is not None and os.path.isfile(CHECKPOINT_PATH):
     checkpoint = torch.load(CHECKPOINT_PATH)
     net.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     start_epoch = checkpoint['epoch']
-    log_string("-> loaded checkpoint %s (epoch: %d)"%(CHECKPOINT_PATH, start_epoch))
+    log_string("-> loaded checkpoint %s (epoch: %d)" % (CHECKPOINT_PATH, start_epoch))
 
 # Decay Batchnorm momentum from 0.5 to 0.999
 # note: pytorch's BN momentum (default 0.1)= 1 - tensorflow's BN momentum
 BN_MOMENTUM_INIT = 0.1
 BN_MOMENTUM_MAX = 0.001
-bn_lbmd = lambda it: max(BN_MOMENTUM_INIT * BN_DECAY_RATE**(int(it / BN_DECAY_STEP)), BN_MOMENTUM_MAX)
-bnm_scheduler = BNMomentumScheduler(net, bn_lambda=bn_lbmd, last_epoch=start_epoch-1)
+bn_lbmd = lambda it: max(BN_MOMENTUM_INIT * BN_DECAY_RATE ** (int(it / BN_DECAY_STEP)), BN_MOMENTUM_MAX)
+bnm_scheduler = BNMomentumScheduler(net, bn_lambda=bn_lbmd, last_epoch=start_epoch - 1)
+
 
 def get_current_lr(epoch):
     lr = BASE_LEARNING_RATE
-    for i,lr_decay_epoch in enumerate(LR_DECAY_STEPS):
+    for i, lr_decay_epoch in enumerate(LR_DECAY_STEPS):
         if epoch >= lr_decay_epoch:
             lr *= LR_DECAY_RATES[i]
     return lr
+
 
 def adjust_learning_rate(optimizer, epoch):
     lr = get_current_lr(epoch)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
 
 # TFBoard Visualizers
 # TRAIN_VISUALIZER = TfVisualizer(FLAGS, 'train')
@@ -218,22 +232,24 @@ def adjust_learning_rate(optimizer, epoch):
 
 
 # Used for AP calculation
-CONFIG_DICT = {'remove_empty_box':False, 'use_3d_nms':True,
-    'nms_iou':0.25, 'use_old_type_nms':False, 'cls_nms':True,
-    'per_class_proposal': True, 'conf_thresh':0.05,
-    'dataset_config':DATASET_CONFIG}
+CONFIG_DICT = {'remove_empty_box': True, 'use_3d_nms': True,
+               'nms_iou': 0.25, 'use_old_type_nms': False, 'cls_nms': True,
+               'per_class_proposal': True, 'conf_thresh': 0.05,
+               'dataset_config': DATASET_CONFIG}
+
 
 # ------------------------------------------------------------------------- GLOBAL CONFIG END
 
-def train_log(loss_dict, ct): # Where the magic happens
+def train_log(loss_dict, ct):  # Where the magic happens
     wandb.log(loss_dict, step=ct)
     print(f"Loss after " + str(ct).zfill(5) + f" examples: {loss_dict['loss']:.3f}")
 
+
 def train_one_epoch():
-    stat_dict = {} # collect statistics
+    stat_dict = {}  # collect statistics
     adjust_learning_rate(optimizer, EPOCH_CNT)
-    bnm_scheduler.step() # decay BN momentum
-    net.train() # set model to training mode
+    bnm_scheduler.step()  # decay BN momentum
+    net.train()  # set model to training mode
     for batch_idx, batch_data_label in enumerate(TRAIN_DATALOADER):
         for key in batch_data_label:
             batch_data_label[key] = batch_data_label[key].to(device)
@@ -242,10 +258,10 @@ def train_one_epoch():
         optimizer.zero_grad()
         inputs = {'point_clouds': batch_data_label['point_clouds']}
         end_points = net(inputs)
-        
+
         # Compute loss and gradients, update parameters.
         for key in batch_data_label:
-            assert(key not in end_points)
+            assert (key not in end_points)
             end_points[key] = batch_data_label[key]
         loss, end_points = criterion(end_points, DATASET_CONFIG)
         loss.backward()
@@ -258,30 +274,30 @@ def train_one_epoch():
                 stat_dict[key] += end_points[key].item()
 
         batch_interval = 10
-        if (batch_idx+1) % batch_interval == 0:
-            log_string(' ---- batch: %03d ----' % (batch_idx+1))
+        if (batch_idx + 1) % batch_interval == 0:
+            log_string(' ---- batch: %03d ----' % (batch_idx + 1))
             loss_dict = {key: stat_dict[key] / batch_interval for key in stat_dict}
-            loss_dict['epoch'] = EPOCH_CNT # TODO: not sure if this is correct
-            example_ct = (EPOCH_CNT*len(TRAIN_DATALOADER)+batch_idx)*BATCH_SIZE
+            loss_dict['epoch'] = EPOCH_CNT  # TODO: not sure if this is correct
+            example_ct = (EPOCH_CNT * len(TRAIN_DATALOADER) + batch_idx) * BATCH_SIZE
             train_log(loss_dict, example_ct)
             # TRAIN_VISUALIZER.log_scalars({key:stat_dict[key]/batch_interval for key in stat_dict},
             #     (EPOCH_CNT*len(TRAIN_DATALOADER)+batch_idx)*BATCH_SIZE)
             for key in sorted(stat_dict.keys()):
-                log_string('mean %s: %f'%(key, stat_dict[key]/batch_interval))
+                log_string('mean %s: %f' % (key, stat_dict[key] / batch_interval))
                 stat_dict[key] = 0
 
 
 def evaluate_one_epoch():
-    stat_dict = {} # collect statistics
-    ap_calculator = APCalculator(ap_iou_thresh=FLAGS.ap_iou_thresh,
-        class2type_map=DATASET_CONFIG.class2type)
-    net.eval() # set model to eval mode (for bn and dp)
+    stat_dict = {}  # collect statistics
+    ap_calculator = VotenetAPCalculator(ap_iou_thresh=FLAGS.ap_iou_thresh,
+                                 class2type_map=DATASET_CONFIG.class2type)
+    net.eval()  # set model to eval mode (for bn and dp)
     for batch_idx, batch_data_label in enumerate(TEST_DATALOADER):
         if batch_idx % 10 == 0:
-            print('Eval batch: %d'%(batch_idx))
+            print('Eval batch: %d' % (batch_idx))
         for key in batch_data_label:
             batch_data_label[key] = batch_data_label[key].to(device)
-        
+
         # Forward pass
         inputs = {'point_clouds': batch_data_label['point_clouds']}
         with torch.no_grad():
@@ -289,7 +305,7 @@ def evaluate_one_epoch():
 
         # Compute loss
         for key in batch_data_label:
-            assert(key not in end_points)
+            assert (key not in end_points)
             end_points[key] = batch_data_label[key]
         loss, end_points = criterion(end_points, DATASET_CONFIG)
 
@@ -299,33 +315,33 @@ def evaluate_one_epoch():
                 if key not in stat_dict: stat_dict[key] = 0
                 stat_dict[key] += end_points[key].item()
 
-        batch_pred_map_cls = parse_predictions(end_points, CONFIG_DICT) 
-        batch_gt_map_cls = parse_groundtruths(end_points, CONFIG_DICT) 
+        batch_pred_map_cls = parse_predictions(end_points, CONFIG_DICT)
+        batch_gt_map_cls = parse_groundtruths(end_points, CONFIG_DICT)
         ap_calculator.step(batch_pred_map_cls, batch_gt_map_cls)
 
         # Dump evaluation results for visualization
-        if FLAGS.dump_results and batch_idx == 0 and EPOCH_CNT %10 == 0:
-            MODEL.dump_results(end_points, DUMP_DIR, DATASET_CONFIG) 
+        if FLAGS.dump_results and batch_idx == 0 and EPOCH_CNT % 10 == 0:
+            MODEL.dump_results(end_points, DUMP_DIR, DATASET_CONFIG)
 
-    # # Log statistics
+            # # Log statistics
     # TEST_VISUALIZER.log_scalars({key:stat_dict[key]/float(batch_idx+1) for key in stat_dict},
     #     (EPOCH_CNT+1)*len(TRAIN_DATALOADER)*BATCH_SIZE)
 
     for key in sorted(stat_dict.keys()):
-        log_string('eval mean %s: %f'%(key, stat_dict[key]/(float(batch_idx+1))))
+        log_string('eval mean %s: %f' % (key, stat_dict[key] / (float(batch_idx + 1))))
 
     # Evaluate average precision
     metrics_dict = ap_calculator.compute_metrics()
     for key in metrics_dict:
-        log_string('eval %s: %f'%(key, metrics_dict[key]))
-    
+        log_string('eval %s: %f' % (key, metrics_dict[key]))
+
     wandb.log(metrics_dict)
-    mean_loss = stat_dict['loss']/float(batch_idx+1)
+    mean_loss = stat_dict['loss'] / float(batch_idx + 1)
     return mean_loss
 
 
 def train(start_epoch):
-    global EPOCH_CNT 
+    global EPOCH_CNT
     min_loss = 1e10
     loss = 0
     # start a new experiment
@@ -333,30 +349,31 @@ def train(start_epoch):
     # hyperparameters = get_hyperparameters() # TODO: implement this
     # tell wandb to get started
     with wandb.init(project="votenet-model-apple", entity="pmlr-votenet"):
-      # access all HPs through wandb.config, so logging matches execution!
-      wandb.watch(net)
-      for epoch in range(start_epoch, MAX_EPOCH):
-        EPOCH_CNT = epoch
-        log_string('**** EPOCH %03d ****' % (epoch))
-        log_string('Current learning rate: %f'%(get_current_lr(epoch)))
-        log_string('Current BN decay momentum: %f'%(bnm_scheduler.lmbd(bnm_scheduler.last_epoch)))
-        log_string(str(datetime.now()))
-	# Reset numpy seed.
-	# REF: https://github.com/pytorch/pytorch/issues/5059
-        np.random.seed()
-        train_one_epoch()
-        if EPOCH_CNT == 0 or EPOCH_CNT % 10 == 9: # Eval every 10 epochs
-          loss = evaluate_one_epoch()
-        # Save checkpoint
-        save_dict = {'epoch': epoch+1, # after training one epoch, the start_epoch should be epoch+1
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'loss': loss,
-                    }
-        try: # with nn.DataParallel() the net is added as a submodule of DataParallel
-            save_dict['model_state_dict'] = net.module.state_dict()
-        except:
-            save_dict['model_state_dict'] = net.state_dict()
-        torch.save(save_dict, os.path.join(LOG_DIR, 'checkpoint.tar'))
+        # access all HPs through wandb.config, so logging matches execution!
+        wandb.watch(net)
+        for epoch in range(start_epoch, MAX_EPOCH):
+            EPOCH_CNT = epoch
+            log_string('**** EPOCH %03d ****' % (epoch))
+            log_string('Current learning rate: %f' % (get_current_lr(epoch)))
+            log_string('Current BN decay momentum: %f' % (bnm_scheduler.lmbd(bnm_scheduler.last_epoch)))
+            log_string(str(datetime.now()))
+            # Reset numpy seed.
+            # REF: https://github.com/pytorch/pytorch/issues/5059
+            np.random.seed()
+            train_one_epoch()
+            if EPOCH_CNT == 0 or EPOCH_CNT % 10 == 9:  # Eval every 10 epochs
+                loss = evaluate_one_epoch()
+            # Save checkpoint
+            save_dict = {'epoch': epoch + 1,  # after training one epoch, the start_epoch should be epoch+1
+                         'optimizer_state_dict': optimizer.state_dict(),
+                         'loss': loss,
+                         }
+            try:  # with nn.DataParallel() the net is added as a submodule of DataParallel
+                save_dict['model_state_dict'] = net.module.state_dict()
+            except:
+                save_dict['model_state_dict'] = net.state_dict()
+            torch.save(save_dict, os.path.join(LOG_DIR, 'checkpoint.tar'))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     train(start_epoch)
